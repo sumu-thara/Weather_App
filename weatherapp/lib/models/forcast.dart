70e90e14d183a612bea1dc0afec15468879893c2
sumudu.2018348@iit.ac.lk <sumudu.2018348@iit.ac.lk>
2023-06-14 18:18:04 +0530

    first commit

---
+++ weatherapp/lib/models/forcast.dart
@@ -0,0 +1,33 @@
+
+import 'package:weatherapp/models/daily.dart';
+import 'package:weatherapp/models/hourly.dart';
+
+class Forecast {
+  final List<Hourly> hourly;
+  final List<Daily> daily;
+
+  Forecast({required this.hourly, required this.daily});
+
+  factory Forecast.fromJson(Map<String, dynamic> json) {
+    List<dynamic> hourlyData = json['hourly'];
+    List<dynamic> dailyData = json['daily'];
+
+    List<Hourly> hourly = new List<Hourly>();
+    List<Daily> daily = new List<Daily>();
+
+    hourlyData.forEach((item) {
+      var hour = Hourly.fromJson(item);
+      hourly.add(hour);
+    });
+    
+    dailyData.forEach((item) {
+      var day = Daily.fromJson(item);
+      daily.add(day);
+    });
+    
+    return Forecast(
+      hourly: hourly,
+      daily: daily
+    );
+  }
+}
